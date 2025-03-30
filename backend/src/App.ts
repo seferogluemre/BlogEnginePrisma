f7ece7108b0d11fd832689e336dd7142c91c4719
seferogluemre <157003672+xEmre1-53@users.noreply.github.com>
2025-03-30 12:46:51 +0300

    server ratelimit,cors configuration settings writing error logger util

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -7,14 +7,17 @@ import tag_routes from './routes/tag_routes'
 import postTag_routes from './routes/postTag_routes'
 import user_routes from './routes/user_routes'
 import dotenv from 'dotenv'
+import { corsOptions } from './config/corsOption'
+import { globalLimiter } from './config/rateLimitConfig'
 
 dotenv.config();
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors(corsOptions));
+app.use(globalLimiter)
 
 app.use('/api/users', user_routes)
 app.use('/api/categories', category_routes)
@@ -23,6 +26,6 @@ app.use('/api/comments', comment_routes)
 app.use('/api/tags', tag_routes)
 app.use('/api/posts', postTag_routes)
 
-app.listen(process.env.PORT || port, () => {
+app.listen(port, () => {
     console.log(`Sunucu Ayakta!!! ${port}`)
 })
