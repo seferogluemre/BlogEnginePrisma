38b1b737ae3c1e3d8a6ad6f8158326fe64668499
seferogluemre <yunusemre5331@icloud.com>
2025-03-31 13:34:30 +0300

    started frontend project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Button variant="contained" className='bg-red'>Hello world</Button>
+    </>
+  )
+}
+
+export default App
