221d15a84a61709c85fa9cc00c19540a9d7219db
seferogluemre <yunusemre5331@icloud.com>
2025-03-31 12:56:35 +0300

    remove frontend project

--- frontend/src/routes/PostDetail/postDetailLoader.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const baseUrl = "http://localhost:3000"
-
-export async function postDetailLoader({ params }) {
-    const response = await axios.get(`${baseUrl}/posts/${params.postId}`)
-    return { post: response.data, comments: response.data.post_comments };
-}
--- frontend/src/routes/posts/Posts_loader.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const baseUrl = "http://localhost:3000"
-
-export async function postsLoader() {
-    const response = await axios.get(`${baseUrl}/posts`)
-    return { posts: response.data.data };
-}
